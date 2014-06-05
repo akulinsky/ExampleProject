866077169c603d7324f27dfa3dd7f4918bf267bf
akulinsky <andrey.kulinskiy@gamil.com>
2014-06-05 12:14:54 +0300

    add MainViewController

---
+++ ExampleProject/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  ExampleProject
+//
+//  Created by Andrey Kulinskiy on 6/5/14.
+//  Copyright (c) 2014 Andrey Kulinskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ ExampleProject/MainViewController.m
@@ -0,0 +1,76 @@
+//
+//  MainViewController.m
+//  ExampleProject
+//
+//  Created by Andrey Kulinskiy on 6/5/14.
+//  Copyright (c) 2014 Andrey Kulinskiy. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@property (nonatomic, strong)UILabel* lblTitle;
+
+@end
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor grayColor];
+    
+    [self performSelectorOnMainThread:@selector(addAllViews) withObject:nil waitUntilDone:NO];
+}
+
+- (void)addAllViews
+{
+    [self.view addSubview:self.lblTitle];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - property
+- (UILabel *)lblTitle
+{
+    if (!_lblTitle) {
+        //_lblWeek = [[UILabel alloc]initWithFrame: self.panelSelectWeek.bounds];
+        _lblTitle = [[UILabel alloc]initWithFrame: CGRectMake(0, 50, self.view.frame.size.width, 50)];
+        _lblTitle.textAlignment = NSTextAlignmentCenter;
+        //_lblWeek.backgroundColor = [Utils colorYellow];
+        _lblTitle.textColor = [UIColor whiteColor];
+        _lblTitle.text = @"Main Branch";
+        //_lblWeek.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        _lblTitle.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+    }
+    return _lblTitle;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
