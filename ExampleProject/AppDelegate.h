6fcaf5d147f2c004ae887e39eb9c0bd73148dd27
akulinsky <andrey.kulinskiy@gamil.com>
2014-06-05 12:02:27 +0300

    Initial Commit

---
+++ ExampleProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ExampleProject
+//
+//  Created by Andrey Kulinskiy on 6/5/14.
+//  Copyright (c) 2014 Andrey Kulinskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
